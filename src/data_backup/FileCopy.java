b01df95b82d4eef98d94e0a16452088e0d739cbe
huangju <931481086@qq.com>
2023-10-08 17:19:41 +0800

    basic framework

--- src/data_backup/FileCopy.java
+++ src/data_backup/FileCopy.java
@@ -3,5 +3,28 @@ package data_backup;
 import java.io.File;
 
 public class FileCopy {
+	public void copy(String src, String dest) {
+		
+	}
+	
+	public void zip(String src, String dest) {
+		
+	}
+	
+	public void unzip(String src, String dest) {
+		
+	}
+	
+	public void encrypy(String src, String dest, int key) {
+		
+	}
+	
+	public void decrypy(String src, String dest, int key) {
+		
+	}
+	
+	public void verify() {
+		
+	}
 
 }
